4af209c3d9c315d4a433c3657109f0d21e2f4eab
mroberge <mroberge.whois@gmail.com>
2016-11-29 21:13:42 -0500

    create activities for module_1

--- README.md
+++ README.md
@@ -14,37 +14,21 @@ coders use every day.
 
 1. Getting Started.
 
-    * Create your own GitHub account.
-    * Fork this repository into your own GitHub account.
-    * Download the repo to your computer in one of these ways:
-        * Install & use the GitHub Desktop Application
-        * or click on the 'Clone or Download' button
-    * Explore the files in this project.
-    * Open module_0.py and run it. (This will also run the tests.)
-    * Try to use the functions in module_0.py from the IPython console.
-        * after running module_0.py inside of IPython, type:
-        `>>> hello_world()`
-        * You can also list all of the functions stored in module_0 this way:
-        `>>> dir(module_0)`
-        * Learn more about the function hello_world():
-        `>>> help(hello_world()`
-    * Try to run the tests. I've set up four different ways:
-        * run module_0 from the IPython interpreter
-        * run test_module_0 from the IPython interpreter
-        * run unittest from the command line:
+    _Make sure that you've completed this part already!_
 
-        `C:\Users\x\Google Drive\PythonWorkspace\learn_python>python -m unittest -v`
-
-        * run the setup file with the test option (setup.py is a file that gets used to create a package):
-
-        `C:\Users\x\Google Drive\PythonWorkspace\learn_python>python setup.py test`
+    * Switch branches to Master to see the instructions for this exercise.
 
 2. A Simple Coding Exercise: module_1.py
 
     _In this module, you'll familiarize yourself with the concept of unit
-    testing and how these modules will work._
+    testing and github._
 
-    * Switch branches to module_1 to get the instructions for this exercise.
+    * Open the file module_1.py
+    * Read the instructions in the docstring for the module.
+    * First, you'll run the file to see how the tests work...
+    * ...and fix a broken function
+    * Then, you'll write your own test.
+    * Make sure that all of the tests pass!
 
 3. Odd-Even Excercise: module_2.py
 
--- learn/module_1.py
+++ learn/module_1.py
@@ -2,17 +2,97 @@
 """
 module_1.py
 
-In the last module, I encouraged you to look around and explore the files
+In the last module, I encouraged you to look around and explore the files. In
+this excercise, you'll get to modify the module and try your own tests.
+
+To make the process easier, I've put all of the code and unittests into the
+same file, module_1.py, so that you can see the test and the code on the same
+page. This is not the normal way to write your tests. Normally, you would
+separate your tests from your code, like I did with module_0 and test_module_0.
+Unfortunately, the weird setup I use here does not work from the command line.
+When you try to run `python -m unittest` from the command line, unittest only
+looks for tests in modules with names that start with 'test_', so unittest
+won't be able to find the tests in this module. So, you should run the tests
+for this exercise by running the module in the interpreter.
+
+Your first job is to fix the function fix_me() so that it can pass the test.
+
+Your second job is to write a test that check whether fix_me() returns
+a string. I put a comment in the place where you should put your test.
+
+When you are done, save it all, commit it, and upload it to Github!
 """
+import unittest
 
 
+def add_five(value):
+    """
+    Returns a value plus five.
+
+    Args:
+    -----
+        value (int): an integer or float.
+
+    Returns:
+    --------
+        output: an integer or float that is five larger than the input value.
+    """
+
+    # For fun, alter this next line to add 6, and watch the tests fail.
+    output = value + 5
+
+    return output
 
 
+def fix_me(name):
+    """Returns a string that says hello.
+
+    Args:
+    -----
+        name (str): the name of a person.
+
+    Returns:
+    --------
+        output: a string that says, 'hello, <name>!'
+    """
+    output = 'Fix me ' + str(name) + '!'
+    return output
 
 
+class TestModule1(unittest.TestCase):
 
+    def test_001_something_stupid(self):
+        """Check: does 5 equal 5?
+        """
+        expected = 5
+        actual = 5
+        self.assertEqual(actual, expected)
 
+    def test_6_add_five_returns_11(self):
+        """Check that add_five(6) returns 11.
+        """
+        expected = 11
+        actual = add_five(6)
+        self.assertEqual(actual, expected, msg='Returned {}'.format(actual))
 
+    def test_whether_add_five_returns_an_integer(self):
+        """Check that add_five() returns an integer.
+        """
+        some_int = 5
+        actual = add_five(some_int)
+        self.assertIsInstance(actual, int, msg='Returned {0}, which'
+                              ' is an instance of {1}'.format(actual,
+                                                              type(actual)))
+
+    def test_fix_me_Marty_returns_hello_Marty(self):
+        """Check that fix_me('Marty') returns 'Hello, Marty'
+        """
+        expected = 'Hello, Marty!'
+        actual = fix_me('Marty')
+        self.assertEqual(actual, expected)
+
+    # Write a test that checks that fix_me returns a string.
+    # def put_the_test_here!
 
 
 if __name__ == '__main__':
@@ -22,4 +102,4 @@ if __name__ == '__main__':
     # Note that I've altered how I run unittest for this module. This will run
     # all of the tests in this module, but none of the tests in any of the
     # other modules.
-    unittest.main(verbosity=2)
+    unittest.main(module='module_1', verbosity=2)
